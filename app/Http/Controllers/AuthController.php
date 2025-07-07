c2096a19d8fe335ec2bba4e5354dc396463d7660
JhonDev12 <jhonsenapro@gmail.com>
2025-07-07 13:44:50 -0500

    implementacion de Sanctum  al proyecto

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+       
+        $request->validate([
+            'email'    => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['message' => 'Credenciales incorrectas.'], 401);
+        }
+
+        return response()->json([
+            'message' => 'Inicio de sesión exitoso.',
+            'user'    => $user,
+            'rol'     => $user->rol_id
+        ]);
+    }
+}
